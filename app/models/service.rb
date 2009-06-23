65a0a67bf6445434dc637cd9b352e3fcf86751ba
Bob <bob@electricgraffitti.com>
2009-06-23 10:40:38 -0600

    commit for branch

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: services
+#
+#  id                  :integer(4)      not null, primary key
+#  title               :string(255)
+#  description         :text
+#  created_at          :datetime
+#  updated_at          :datetime
+#  avatar_file_name    :string(255)
+#  avatar_content_type :string(255)
+#  avatar_file_size    :integer(4)
+#  avatar_updated_at   :datetime
+#  keywords            :string(255)
+#  permalink           :string(255)
+#
+
 class Service < ActiveRecord::Base
   
   # Thinking Sphinx Indexes
