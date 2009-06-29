0f0d491a73192bb379d1f9d153bf5c76f0995d11
Bob <bob@electricgraffitti.com>
2009-06-29 08:35:21 -0600

    changed to info@hcinsight.com

--- app/models/application_mailer.rb
+++ app/models/application_mailer.rb
@@ -1,7 +1,7 @@
 class ApplicationMailer < ActionMailer::Base
   
   def contact_mailer(contact, sent_at = Time.now)
-      recipients    "support@hcinsight.com"
+      recipients    "info@hcinsight.com"
       from          "HealthCare Insight :: Contact Us"
       subject       contact[:contact_us_subject]
       body          :contact => contact, :sent_on => sent_at
