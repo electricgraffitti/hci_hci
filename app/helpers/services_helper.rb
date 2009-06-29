d02078be53a0cd522b2683ef279815e9f8011532
Bob <bob@electricgraffitti.com>
2009-06-29 15:03:51 -0600

    tweaks

--- app/helpers/services_helper.rb
+++ app/helpers/services_helper.rb
@@ -5,7 +5,7 @@ module ServicesHelper
     a << "<div class='grid_2 alpha '>#{link_to image_tag(serv.avatar.url(:small), :alt => serv.title), service_path(serv)}</div>"
     a << "<div class='grid_10 omega'>"
     a << "<h3>#{serv.title}&#8482</h3>"
-    a << "<p>#{serv.description}</p>"
+    a << "<p>#{truncate(serv.description, :length => 455)}</p>"
     a << "</div></div>"
   end
   
