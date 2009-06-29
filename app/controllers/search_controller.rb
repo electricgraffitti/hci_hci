72211831756f243872f4c329b27f20b4d228afc2
Bob <bob@electricgraffitti.com>
2009-06-29 12:48:51 -0600

    adjustments to search results

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,6 +4,11 @@ class SearchController < ApplicationController
   
   def index
     @services = Service.search params[:search]
+    @articles = Article.search params[:search]
+    @documents = Document.search params[:search]
+    @events = Event.search params[:search]
+    @links = Link.search params[:search]
+    # add more sources as needed
   end
 
 end
