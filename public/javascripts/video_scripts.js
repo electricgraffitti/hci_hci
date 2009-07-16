b30231150ec368c4c92c8c358eb02cddc5de560f
Bob <bob@electricgraffitti.com>
2009-07-16 13:59:02 -0600

    rough in for social media links at the top

--- public/javascripts/video_scripts.js
+++ public/javascripts/video_scripts.js
@@ -1,4 +1,9 @@
 // This sets up the videos using FlowPlayer
 $(document).ready(function() {
-    flowplayer("home_player", "/swfs/flowplayer-3.1.1.swf", "/videos/test.f4v");
+    flowplayer("home_player", "/swfs/flowplayer-3.1.1.swf", "/videos/test.f4v", {
+    clip: {
+      autoPlay: false,
+      autoBuffering: true
+    }
+  });
 });
