ce27cc855c90436a8d671e503c1b1e51346af327
Bob <bob@electricgraffitti.com>
2009-08-03 14:58:20 -0600

    adjusted priority form

--- public/javascripts/video_scripts.js
+++ public/javascripts/video_scripts.js
@@ -1,4 +1,23 @@
 // This sets up the videos using FlowPlayer
 $(document).ready(function() {
-    flowplayer("home_player", "/swfs/flowplayer-3.1.1.swf", "/videos/test.f4v");
+    flowplayer("home_player", "/swfs/flowplayer-3.1.1.swf", {
+      clip: {
+        url: "/videos/test.f4v",
+        autoPlay: false
+        }
+    });
 });
+
+// $f("player1", "flowplayer.swf", { 
+//     clip: { 
+//         url: 'KimAronson-TwentySeconds59483.flv', 
+//         autoPlay: true, 
+//         autoBuffering: true 
+//     }, 
+//     plugins: { 
+//         controls: null 
+//     }, 
+//     onLoad: function(){ 
+//         alert("player loaded"); 
+//     } 
+// });
