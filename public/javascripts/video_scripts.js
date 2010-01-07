c78fb6f118e3c51f202e8dc5e808e76eef773c17
Bob <bob@electricgraffitti.com>
2010-01-07 13:26:03 -0700

    added the nucleus video trickery

--- public/javascripts/video_scripts.js
+++ public/javascripts/video_scripts.js
@@ -3,7 +3,7 @@
 // Nucleus Banner1 Video
 
 $(document).ready(function() {
-    flowplayer("nucleus_main_video", 
+    var player = $f("nucleus_main_video", 
     // Flash Configs
     {
       src: "/swfs/flowplayer.commercial-3.1.5.swf",
@@ -17,17 +17,51 @@ $(document).ready(function() {
       clip: {
         url: "/videos/meet_nucleus_1.f4v",
         autoPlay: true,
-        backgroundColor: 'transparent'
+        backgroundColor: 'transparent',
+        
+        onLoad: function() {
+    			player.play();
+    		},
+        
+        onPause: function(){ 
+          player.hide();
+          document.getElementById("nucleus_main_video").style.display="none";
+        },
+        
+        onStop: function(){ 
+          player.hide();
+          document.getElementById("nucleus_main_video").style.display="none";
+        },
+        
+        onFinish: function(){ 
+          player.hide();
+          document.getElementById("nucleus_main_video").style.display="none";
+        }
+        
       },
       play: { 
-          label: null, 
-          replayLabel: "Get Nucleus" 
+          label: null
       },
       plugins:  { 
         controls: null 
       }
     });
+    
+    $('.play_again_link').click(function() {
+      document.getElementById("nucleus_main_video").style.display="";
+      player.show();
+      player.load();
+      player.play();
+      return false;
+    });
+    
 });
+
+
+// player.onPause(function() {
+//   // $('.banner_li a.li_2').trigger('click');
+//   alert('got here');
+// });
 
 // Nucleus Banner2 Video
 
