a7f85cdc85a1c2f02546429940b45a3dc384caaa
Bob <bob@electricgraffitti.com>
2010-01-21 10:19:26 -0700

    videos order

--- app/controllers/resources_controller.rb
+++ app/controllers/resources_controller.rb
@@ -5,7 +5,7 @@ class ResourcesController < ApplicationController
     @articles = Article.all.paginate :per_page => 6, :page => params[:page], :order => 'created_at DESC'
     @documents = Document.brochures
     @press_releases = Article.type('press_release').small_list(3)
-    @videos = Video.all
+    @videos = Video.list(4, params[:page])
     @events = Event.all
   end
 
