8c63cdea9bcd84313a1de0f46a49935437bc5099
Bob Hanson <bob@bobmac.hcinsight.net>
2010-10-25 15:39:47 -0600

    articles rss

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,13 +7,14 @@ class ArticlesController < ApplicationController
   def index
     # @articles = Article.type(params[:article_type])
     @articles = Article.no_press.paginate :per_page => 6, :page => params[:page]
+    @articlesrss = Article.all
     @press_releases = Article.type('press_release').small_list(5)
     @events = Event.upcoming_events.small_list(1).last_created
     @advertisements = Advertisement.current_list.small_list(2).order_list
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @articles }
-      format.rss { render :rss => @articles }
+      format.rss { render :rss => @articlesrss }
       format.atom
     end
   end
