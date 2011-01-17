de623a8c09b88a12f54eeb71c7ea1d8741617968
Bob Hanson <bob@cube2media.com>
2011-01-17 13:17:31 -0700

    updating to 2.3.9

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file
 
 # Specifies gem version of Rails to use when vendor/rails is not present
-RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
+RAILS_GEM_VERSION = '2.3.9' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
@@ -19,16 +19,12 @@ Rails::Initializer.run do |config|
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
-  config.gem 'RedCloth', :version => ">= 4.2.3", :source => 'http://gemcutter.org'
+  config.gem 'RedCloth'
   config.gem "authlogic"
   # config.gem 'bullet', :source => 'http://gemcutter.org'
-  config.gem 'will_paginate', :version => '~> 2.3.15', :source => 'http://gemcutter.org'
+  config.gem 'will_paginate'
   # config.gem "active_youtube"
-  config.gem(
-    'thinking-sphinx',
-    :lib     => 'thinking_sphinx',
-    :version => '1.3.20'
-    )
+  config.gem 'thinking-sphinx'
     # 
     # ENV["MAGICK_CODER_MODULE_PATH"]="<installdir>/common/lib/ImageMagick-6.6.10-0/modules-Q16/coders"
     # ENV["GS_FONTPATH"] = "<installdir>/common/share/ghostscript/fonts"
